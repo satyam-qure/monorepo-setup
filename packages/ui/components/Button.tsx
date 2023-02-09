c12327bd1a0e974332ec9d730ad83a12d88b8e7a
Satyam Dahlan <satyam.dahlan@qure.ai>
2023-02-09 15:18:20 +0530

    initial stup pushed

---
+++ packages/ui/components/Button.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+export const Button = () => {
+  return (
+    <button className="cursor-pointer rounded-md border border-gray-600 px-2 py-1 text-base text-gray-800 hover:bg-gray-600 hover:text-white">
+      Boop
+    </button>
+  );
+};
